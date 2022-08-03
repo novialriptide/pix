d68e8d61848b03bc65c44b40b5c39eb1d26428a9
Andrew Hong <andrewhong@myyahoo.com>
2022-08-03 15:53:46 -0400

    Moved states aroudn

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pxdart/pxdart.dart';
 
+import 'states/homepage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,137 +10,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'nakiapp',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'nakiapp'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    String sortDropDownValue = 'popular_desc';
-    String durationDropDownValue = 'none';
-
-    PixivClient pxdartClient = PixivClient();
-    pxdartClient.connect("jSC-iVbHPw6-HZckMLpOrh7FbPohFLRa_7JoqNIxAVk");
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a keyword/ID',
-                ),
-              ),
-            ),
-            Column(children: <Widget>[
-              Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: DropdownButtonFormField<String>(
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          labelStyle: TextStyle(),
-                          labelText: 'Sort'),
-                      key: const Key('sort'),
-                      value: sortDropDownValue,
-                      items: <String>['popular_desc', 'date_desc', 'date_asc']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {})),
-              Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: DropdownButtonFormField<String>(
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          labelStyle: TextStyle(),
-                          labelText: 'Duration'),
-                      key: const Key('duration'),
-                      value: durationDropDownValue,
-                      items: <String>[
-                        'within_last_day',
-                        'within_last_week',
-                        'within_last_month',
-                        'none'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {}))
-            ])
-          ],
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'nakiapp',
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Search',
-        child: const Icon(Icons.search),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: const HomePage());
   }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State createState() => HomePageState();
 }
---
+++ lib/states/seachoptions.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class SearchOptionsState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String sortDropDownValue = 'popular_desc';
+    String durationDropDownValue = 'none';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: Text('nakiapp'),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 400,
+                child: Center(
+                  child: Column(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              labelStyle: TextStyle(),
+                              labelText: 'Sort'),
+                          key: const Key('sort'),
+                          value: sortDropDownValue,
+                          items: <String>[
+                            'popular_desc',
+                            'date_desc',
+                            'date_asc'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (value) => {}),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              labelStyle: TextStyle(),
+                              labelText: 'Duration'),
+                          key: const Key('duration'),
+                          value: durationDropDownValue,
+                          items: <String>[
+                            'none',
+                            'within_last_day',
+                            'within_last_week',
+                            'within_last_month'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (value) => {}),
+                    )
+                  ]),
+                ),
+              )
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}

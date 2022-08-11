fe86698fd3949e6c090e48cdcc10be1f94112bab
Andrew Hong <andrewhong@myyahoo.com>
2022-08-11 03:45:41 -0400

    more action stuff

--- lib/states/illustview.dart
+++ lib/states/illustview.dart
@@ -55,84 +55,87 @@ class IllustViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(illust.title)),
+      appBar: AppBar(
+          title: Text(illust.title),
+          shape: const ContinuousRectangleBorder(),
+          actions: [
+            IconButton(icon: const Icon(Icons.file_download), onPressed: () {})
+          ]),
       body: SingleChildScrollView(
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        InkWell(
-            child: FutureBuilder<Uint8List>(
-                future: getImage(),
-                builder:
-                    (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
-                  if (snapshot.hasData) {
-                    return Image.memory(snapshot.data!);
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                })),
+        FutureBuilder<Uint8List>(
+            future: getImage(),
+            builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+              if (snapshot.hasData) {
+                return InkWell(
+                    onTap: () {
+                      // Show image in full screen
+                    },
+                    child: Image.memory(snapshot.data!));
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
         Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                    padding: const EdgeInsets.only(bottom: 5.0),
-                    child: Row(children: [
-                      InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SearchScreen()));
-                          },
-                          child: Chip(
-                              avatar: FutureBuilder<Uint8List>(
-                                  future: getUserImage(),
-                                  builder: (BuildContext context,
-                                      AsyncSnapshot<Uint8List> snapshot) {
-                                    if (snapshot.hasData) {
-                                      return ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(100.0),
-                                          child: Image.memory(snapshot.data!));
-                                    } else {
-                                      return const Center(
-                                          child: CircularProgressIndicator());
-                                    }
-                                  }),
-                              label: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(illust.displayName),
-                                    Text(illust.userName,
-                                        style: const TextStyle(fontSize: 13.5))
-                                  ]))),
-                      const Spacer(),
-                      InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SearchScreen()));
-                          },
-                          child: const Chip(label: Text("Follow"))),
-                    ])),
-                Text(illust.title,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 17)),
-                Text(illust.caption),
-                Wrap(
-                    spacing: 5.0,
-                    runSpacing: -7.0,
-                    direction: Axis.horizontal,
-                    children: getTagsAsWidgets()),
-              ],
-            ))
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 5.0),
+                  child: Row(children: [
+                    InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SearchScreen()));
+                        },
+                        child: Chip(
+                            avatar: FutureBuilder<Uint8List>(
+                                future: getUserImage(),
+                                builder: (BuildContext context,
+                                    AsyncSnapshot<Uint8List> snapshot) {
+                                  if (snapshot.hasData) {
+                                    return ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(100.0),
+                                        child: Image.memory(snapshot.data!));
+                                  } else {
+                                    return const Center(
+                                        child: CircularProgressIndicator());
+                                  }
+                                }),
+                            label: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(illust.displayName),
+                                  Text(illust.userName,
+                                      style: const TextStyle(fontSize: 13.5))
+                                ]))),
+                    const Spacer(),
+                    InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SearchScreen()));
+                        },
+                        child: const Chip(label: Text("Follow"))),
+                  ])),
+              Text(illust.title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 17)),
+              Text(illust.caption),
+              Wrap(
+                  spacing: 5.0,
+                  runSpacing: -7.0,
+                  direction: Axis.horizontal,
+                  children: getTagsAsWidgets()),
+            ]))
       ])),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
+        onPressed: () {},
         backgroundColor: Colors.lightBlueAccent,
         child: const Icon(Icons.favorite),
       ),

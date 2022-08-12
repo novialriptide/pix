3ae9f77d94fc5a7e41467fb7e4cd9286d537aa92
Andrew Hong <andrewhong@myyahoo.com>
2022-08-11 20:35:34 -0400

    Cleanup

--- lib/states/homepage.dart
+++ lib/states/homepage.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:nakiapp/globals.dart';
 import 'package:nakiapp/states/illustview.dart';
@@ -21,15 +22,16 @@ class SearchState extends State<SearchScreen> {
   List<Uint8List> images = [];
   List<int> imageIds = [];
   int noPremiumPopularityIndex = 0;
-  final controller = ScrollController();
+  final scrollController = ScrollController();
+  final textController = TextEditingController();
 
   @override
   void initState() {
     client = PixivClient();
     client.connect(refreshToken);
-    controller.addListener(() {
-      if (controller.position.atEdge) {
-        bool isTop = controller.position.pixels == 0;
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        bool isTop = scrollController.position.pixels == 0;
         if (noPremiumPopularityIndex > imageIds.length) {
           hasMore = false;
         }
@@ -115,68 +117,73 @@ class SearchState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Container(
-            width: double.infinity,
-            height: 40,
-            color: Colors.white,
-            child: Center(
-              child: TextField(
-                  onSubmitted: (value) {
-                    searchKeyTerm = value;
-                    images = [];
-                    imageIds = [];
-                    cachedIllusts = [];
-                    loadImages(value);
-                  },
-                  decoration: const InputDecoration(
-                      hintText: 'Search keyterm/ID',
-                      prefixIcon: Icon(Icons.search))),
+        appBar: AppBar(
+            title: Container(
+              width: double.infinity,
+              height: 40,
+              color: Colors.white,
+              child: Center(
+                child: TextField(
+                    controller: textController,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (_) {
+                      searchKeyTerm = textController.text;
+                      images = [];
+                      imageIds = [];
+                      cachedIllusts = [];
+                      loadImages(textController.text);
+                    },
+                    decoration: const InputDecoration(
+                        hintText: 'Search keyterm/ID',
+                        prefixIcon: Icon(Icons.search))),
+              ),
             ),
-          ),
-          shape: const ContinuousRectangleBorder(),
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SearchOptions()));
-                })
-          ]),
-      body: GridView.builder(
-          controller: controller,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            mainAxisSpacing: 5,
-            crossAxisSpacing: 10,
-            crossAxisCount: 3,
-          ),
-          itemCount: images.length + 1,
-          itemBuilder: (context, index) {
-            if (index < images.length) {
-              final illust = InkWell(
-                  onTap: () {
+            shape: const ContinuousRectangleBorder(),
+            actions: [
+              IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => IllustViewScreen(
-                                illust: cachedIllusts[index], client: client)));
-                  },
-                  child: Column(children: [
-                    ClipRRect(
-                        borderRadius: BorderRadius.circular(32.0),
-                        child: Image.memory(images[index]))
-                  ]));
-              return illust;
-            } else {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Center(
-                    child: isLoadingMore
-                        ? const CircularProgressIndicator()
-                        : Container()),
-              );
-            }
-          }),
-    );
+                            builder: (context) => SearchOptions()));
+                  })
+            ]),
+        body: GridView.builder(
+            controller: scrollController,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 10,
+              crossAxisCount: 3,
+            ),
+            itemCount: images.length + 1,
+            itemBuilder: (context, index) {
+              if (index < images.length) {
+                final illust = InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => IllustViewScreen(
+                                  illust: cachedIllusts[index],
+                                  client: client)));
+                    },
+                    child: Column(children: [
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(32.0),
+                          child: Image.memory(images[index]))
+                    ]));
+                return illust;
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                      child: isLoadingMore
+                          ? const CircularProgressIndicator()
+                          : Container()),
+                );
+              }
+            }));
   }
 }

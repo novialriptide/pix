8c262fcf28572acf609be44753a8597bbfb96df0
Andrew Hong <andrewhong@myyahoo.com>
2022-08-16 04:07:20 -0400

    Add some values

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,1 +1,9 @@
 String refreshToken = "";
+
+String clientId = 'MOBrBDS8blbauoSck0ZfDbtuzpyT';
+String clientSecret = 'lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj,. ';
+String loginUserAgent = 'PixivAndroidApp/5.0.234 (Android 11; Pixel 5)';
+String redirectUri =
+    'https://app-api.pixiv.net/web/v1/users/auth/pixiv/callback';
+String loginUrl = 'https://app-api.pixiv.net/web/v1/login';
+String authTokenUrl = 'https://oauth.secure.pixiv.net/auth/token';
